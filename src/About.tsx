68cc387976c3379ba8f086cdd52028c8f14331f1
Veronica Ulrikkeholm <108199135+veronisab@users.noreply.github.com>
2023-08-01 07:52:49 +0200

    Add files via upload

---
+++ src/About.tsx
@@ -0,0 +1,27 @@
+export function About(){
+    return(
+        <div className="content">
+            <div>
+                Veronica Isabelle Ulrikkeholm (born Stubberud) is a Norwegian oboist, currently based in Copenhagen. 
+                She is enjoying a rich freelance career, regularly performing with various orchestras and ensembles in 
+                Norway, Denmark and abroad. She is also a keen contemporary music performer - and as part of her Bachelor project - commisioned 
+                and premiered 8 new works for the oboe from Danish, Norwegian, British, American, Canadian and Argentinian composers.<br />
+                <br />
+
+                She has done an Post-graduate Artist Diploma (supported by the Mimi Opperby Award) at the Royal College of Music, where she 
+                studied with Juliana Koch and Olivier Stankiewicz, Fabien Thouand, Christine Pendrill, and Jacques Tys. She has also studied 
+                with Johannes Grosso and Min Hua Chiu in Oslo, and holds a Master of Music degree from the Royal Danish Academy of Music in 
+                Copenhagen, where she studied with Max Artved and Joakim Dam Thomsen; and a bachelor degree from the Royal Academy of Music 
+                Aarhus, where she studied with Oliver Nordahl. <br />
+                <br />
+
+                She has participated in numerous masterclasses, eg. with Alexei Ogrintchouk, the MURI Masterclasses (with Christoph Hartmann, 
+                Emmanuel Abbühl and Simon Fuchs) and the Klausen Masterclass (with Nick Deutsch and David Walter). Veronica has been awarded 
+                scholarships from Foundation Idella, Annemarie Mortensens Legat, Augustinus Fonden, Wilhelm Hansen Fonden, Aage og Johanne 
+                Louis-Hansens Fond, Drammen Kommunes Kulturstipend and William Demant Fonden. <br />
+                <br />
+
+                Veronica plays a Marigaux M2 oboe and a Marigaux 930 cor anglais.</div>
+        </div>
+    )
+}

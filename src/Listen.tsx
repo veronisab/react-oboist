9d84255cfbd2d8ef7f398885f9db393575b35bf1
Veronica Ulrikkeholm <mail@veronicaisabelle.com>
2023-08-11 20:13:14 +0200

    remove gh-pages

--- src/Listen.tsx
+++ src/Listen.tsx
@@ -2,6 +2,7 @@ export function Listen(){
     return(
         <div className="content">
             <h1>Listen</h1>
+            Youtube
                         <iframe src="https://www.youtube.com/embed/8WaVzA6dxdw" title="CPE Bach Trio Sonata"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                         <iframe src="https://www.youtube.com/embed/K7rsEcYtlTs" title="Poulenc Oboe Sonata"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                         <iframe src="https://www.youtube.com/embed/BXDT2jw1Jxw" title="Dutilleux Oboe Sonata"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
